671ca862e7387d3379d658488897f45f60af0b0f
pauloosmt <paulostaciano@gmail.com>
2025-07-22 22:48:01 -0300

    criando entidade livro

--- src/main/java/com/br/emakers/apiProjeto/data/entity/Livro.java
+++ src/main/java/com/br/emakers/apiProjeto/data/entity/Livro.java
@@ -1,5 +1,31 @@
 package com.br.emakers.apiProjeto.data.entity;
 
-public class Livro {
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "livro")
+
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idLivro;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "autor", nullable = false, length = 100)
+    private String autor;
+
+    @Column(name = "data_lancamento", nullable = false)
+    private LocalDate data_lancamento;
 }
