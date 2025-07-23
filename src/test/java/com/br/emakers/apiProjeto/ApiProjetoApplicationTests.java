0d953fed9424ebd184490bfc4805f6c78e694e97
Paulo <paulostaciano@gmail.com>
2025-07-22 21:28:57 -0300

    git inicial

---
+++ src/test/java/com/br/emakers/apiProjeto/ApiProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.emakers.apiProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
