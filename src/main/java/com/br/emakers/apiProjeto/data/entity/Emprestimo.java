25c753fd6f8a4265f42253803b6904234112df0d
pauloosmt <paulostaciano@gmail.com>
2025-07-23 14:55:47 -0300

    relacionando as 3 tabelas

--- src/main/java/com/br/emakers/apiProjeto/data/entity/Emprestimo.java
+++ src/main/java/com/br/emakers/apiProjeto/data/entity/Emprestimo.java
@@ -1,5 +1,21 @@
 package com.br.emakers.apiProjeto.data.entity;
 
+import jakarta.persistence.*;
+
+
+@Entity
+@IdClass(EmprestimoId.class)
+@Table(name = "emprestimo")
+
 public class Emprestimo {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "idLivro")
+    private Livro livro;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "idPessoa")
+    private Pessoa pessoa;
 
 }
